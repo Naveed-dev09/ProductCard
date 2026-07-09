836e08da81ea865d12939b558fba66eb3c275856
Naveed Hassan <hassan242135@gmail.com>
2026-07-09 12:28:35 +0500

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ function App() {
 
   return (
     <div className="w-full mx-auto px-4 py-8 flex grow">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 ">
 
       {productData.map((product) => (
-        <Card image={product.image}
+        <Card className="mx-auto w-72"
+        image={product.image}
               name={product.name}
               price={product.price}
               desc={product.desc}
